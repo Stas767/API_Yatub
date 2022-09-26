ce9c7c2c41becaecf6171669144d330e889ba4e2
Станислав Балджи <stanislavbaldzhy.yandex.ru>
2022-09-26 11:52:27 +0400

    Добавил роутеры в api.urls

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,19 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='add_comments'
+)
+router.register(r'groups', GroupViewSet)
+router.register(r'follow', FollowViewSet)
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
